a35191770d2f8b626a3a3f66217df85e48750b8f
diayan <diayans@outlook.com>
2023-11-16 10:32:42 +0000

    Initial Commit

---
+++ iOSStarterTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSStarterTemplate
+//
+//  Created by diayan siat on 16/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
